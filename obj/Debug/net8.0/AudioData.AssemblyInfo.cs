f3ddaa3993ab749a9ccb43cf4a83a5840af6a821
Tweaks <51744514+DaTweaks@users.noreply.github.com>
2024-11-21 18:38:20 +0100

    new stuff

--- obj/Debug/net8.0/AudioData.AssemblyInfo.cs
+++ obj/Debug/net8.0/AudioData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AudioData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6416ab0d7dc21f9f5f369e116501b80a3ba0abce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2a818ce980c7cd33495015d3d5ec2c5974e72f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AudioData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AudioData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
